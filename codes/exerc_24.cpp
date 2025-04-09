cd89c51cff189073b9115b3162f67868c7e8bc09
viniciusbduarte <vg39348@gmail.com>
2025-04-09 09:10:29 -0300

    func media, exerc_24

--- codes/exerc_24.cpp
+++ codes/exerc_24.cpp
@@ -1,21 +1,28 @@
-#include <iostream> 
+#include <iostream>
 
-
-double nota1{0};
-double nota2{0};
-double nota3{0};
+double media(double nota1, double nota2, double nota3);
 
 int main(){
+    // nome do aluno
     std::string nome_aluno;
-    std::cout << "Digite o nome do aluno: " ;
+    std::cout << "Digite o nome do aluno: ";
     std::getline(std::cin, nome_aluno);
-    std::cout << "Digite as 3 notas do aluno " << nome_aluno << std::endl;
 
+    // notas
+    double nota1{0}, nota2{0}, nota3{0};
+    std::cout << "Digite as 3 notas do aluno " << nome_aluno << ": ";
     std::cin >> nota1 >> nota2 >> nota3;
 
-    double media = ((nota1*3) + (nota2*2) + (nota3*5))/10;
-
-    std::cout << "A média ponderada do aluno " << nome_aluno << " é " << media << std::endl;
+    // média
+    std::cout << "A aluno " << nome_aluno << " obteve média "
+              << media(nota1, nota2, nota3) << std::endl;
 
     return 0;
+}
+
+// função da média
+double media(double nota1, double nota2, double nota3){
+    int peso_nota1{3}, peso_nota2{4}, peso_nota3{5};
+    return ((nota1 * peso_nota1) + (nota2 * peso_nota2) + (nota3 * peso_nota3)) 
+            / (peso_nota1 + peso_nota2 + peso_nota3);
 }
