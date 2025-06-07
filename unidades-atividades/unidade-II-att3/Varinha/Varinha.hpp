46e7acb72b5479d9f9f707080078d99504640ea8
viniciusbduarte <vg39348@gmail.com>
2025-06-07 14:28:26 -0300

    create varinha

--- unidades-atividades/unidade-II-att3/Varinha/Varinha.hpp
+++ unidades-atividades/unidade-II-att3/Varinha/Varinha.hpp
@@ -1,14 +1,25 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include "Varinha.cpp"
 using namespace std;
 
-class Varinha{
+class Varinha {
 private:
-    string tipo;
+    string tipo_madeira;
     string nucleo;
-    int comprimento;
-public:
+    float comprimento;
 
-}
+public:
+    Varinha();
+    Varinha(string t, string n, float c);
+
+    void setTipoMadeira(string tipo_madeira);
+    void setNucleo(string nucleo);
+    void setComprimento(float comprimento);
+
+    string getTipoMadeira() const;
+    string getNucleo() const;
+    float getComprimento() const;
+
+    void display();
+};
--- unidades-atividades/unidade-II-att3/Varinha/main.cpp
+++ unidades-atividades/unidade-II-att3/Varinha/main.cpp
@@ -4,8 +4,14 @@
 
 
 int main(){
-    
+     
+Varinha varinhaqualquer("qdwadw","dadwadw", 2);
 
+varinhaqualquer.setTipoMadeira("Madeira");
+varinhaqualquer.setNucleo("sla ome");
+varinhaqualquer.setComprimento(2);
+
+varinhaqualquer.display();
 
 
 
