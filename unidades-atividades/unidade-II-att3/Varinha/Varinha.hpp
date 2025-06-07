28ad6a90a9d32b5659d0ce35174f8b34e9536c6c
viniciusbduarte <vg39348@gmail.com>
2025-06-07 12:11:38 -0300

    Prova

---
+++ unidades-atividades/unidade-II-att3/Varinha/Varinha.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Varinha.cpp"
+using namespace std;
+
+class Varinha{
+private:
+    string tipo;
+    string nucleo;
+    int comprimento;
+public:
+
+}
---
+++ unidades-atividades/unidade-II-att3/Varinha/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "Varinha.hpp"
+
+
+int main(){
+    
+
+
+
+
+    return 0;
+}
